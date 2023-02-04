1f12670e28dbb9871e55603dd88330ae4d06afb6
bhabesh2 <jsarma058@gmail.com>
2023-02-04 18:56:53 +0530

    modifed

--- missing Number.js
+++ missing Number.js
@@ -16,3 +16,18 @@ let arr = [1,2,3,5];
 let N = arr.length;
 let ans = missingNum(arr,N);
 console.log(ans);
+
+// 2nd type
+
+let obj={};
+    let miss="",bag=[]
+    for(let i=0;i<arr.length;i++){
+        bag.push(i+1)
+        obj[arr[i]]=0
+    }
+    for(let i=0;i<bag.length;i++){
+        if(obj[bag[i]]==undefined){
+            console.log(bag[i])
+            break;
+        }
+    }
